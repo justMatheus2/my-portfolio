f683e472dfa7f52bc9d40996f25ca5bb19e1dcf8
Matheus Sousa <sousamatheus191999@gmail.com>
2025-04-10 20:07:34 +0100

    add menu-mobile

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,12 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark');
   }
+
+  const menuCheckbox = document.getElementById('menu-hamburguer');
+  const menuLinks = document.querySelectorAll('nav ul a');
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      menuCheckbox.checked = false;
+    });
+  });
