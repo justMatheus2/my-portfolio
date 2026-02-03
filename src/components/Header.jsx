0079751c7029d495e93441d13229cacfd6249a86
Matheus Sousa <sousamatheus191999@gmail.com>
2026-02-03 00:00:11 +0000

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import Nav from "./Nav";
 import "../styles/header.css";
+import logo from "../assets/logoms.png";
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
+        <img src={logo} alt="Matheus logo" className="logoms" 
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })} />
         <span className="logo">Matheus.dev</span>
         <Nav />
       </div>
